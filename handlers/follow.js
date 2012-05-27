afead9cd95f342980732f7bf238a85ba37aa591f
iamlaobie <iamlaobie@gmail.com>
2012-05-27 11:26:38 +0800

    添加关注接口

---
+++ handlers/follow.js
@@ -0,0 +1,73 @@
+var _ = require("underscore");
+var db = require('../lib/db');
+var tool = require('../lib/tool');
+module.exports = {
+    apiAction:"friendships_create",
+
+    receive:function(data){
+        if(!data.accountId || !data.accountId.match(/^\d+$/)){
+            return {message:"accountId's format is not valid", result:"error"};
+        }
+
+        if(!data.weiboUserId || typeof data.weiboUserId != 'string'){
+            return {message:"weiboUserId's format is not valid", result:"error"};
+        }
+
+        var tasks = [];
+        var task = _.clone(data);
+        tasks.push(task);
+        return {result:"ok",tasks:tasks};
+    },
+
+    prepare:function(context, callback){
+        callback(null,  context);
+    },
+
+    finish:function(context, callback){
+        callback(null, context);
+    }, 
+
+    report:function(context){
+        var resp = context.response;
+        var task = context.task;
+
+        var report = {
+            taskId:task.taskId,
+            action:task.action,
+            accountId:task.accountId,
+            result:context.result,
+            fromApp:task.fromApp
+        };
+        report.weiboUserId = task.weiboUserId;
+
+        report.msg = '';
+        if(context.err){
+            report.msg = context.err.message;
+        }
+        return report;
+    },
+
+    log:function(context){
+        var task = context.task;
+        var report = this.report(context);
+        var weiboUserId = task.weiboUserId;
+        return weiboUserId;
+    },
+
+
+    error:function(err, context){
+        if(err.message.match(/timeout/i)){
+            return "retry";
+        }
+
+        if(err.message.match(/^40(308|090|310)/)){
+            return "limit";
+        }
+
+        if(err.message.match(/^400(13|25)/)){
+            return "drop";
+        }
+
+        return "retry";
+    }
+}
